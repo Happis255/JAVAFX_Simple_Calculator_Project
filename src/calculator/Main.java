1ae1dfc64a24bbb85ca6ec94a9121802f977b606
Happis255 <hubertwasik27@hotmail.com>
2020-04-02 14:47:00 +0200

    v0.1.0 - calculating two numbers

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -4,18 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        primaryStage.setTitle("Simple Java Calculator Project v0.1");
+        primaryStage.getIcons().add(new Image("icon.png"));
+        primaryStage.setScene(new Scene(root, 300, 300));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
